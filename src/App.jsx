e76f653cf4178a7567473ebb221fbdcf8f7e2b71
Abdullah Al Ashrafee <dev.ashrafee@gmail.com>
2025-07-29 16:15:42 +0600

    Navbar & Sidebar Implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
 import React from "react";
-import Navbar from "./components/Common/Navbar";
-import Sidebar from "./components/Common/Sidebar";
 import { createBrowserRouter, RouterProvider } from "react-router";
+import Home from "./components/Pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <Home />,
   },
 ]);
 
 const App = () => {
-  return (
-    
-      <RouterProvider router={router} />
-  
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
---
+++ src/components/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar_Sidebar from "../Common/Navbar_Sidebar";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar_Sidebar />
+    </div>
+  );
+};
+
+export default Home;
